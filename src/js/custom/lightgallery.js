c6dfed7edbee4578b764368cd9c36d6e6d56fe9a
mizza-cz <mizza.cz666@gmail.com>
2023-08-08 10:41:02 +0200

    add static/gallery

--- src/js/custom/lightgallery.js
+++ src/js/custom/lightgallery.js
@@ -18,3 +18,13 @@ var scroll = "yes",
       $(document).scrollTop() > s && $(this).addClass("anime");
     });
   }));
+$(function () {
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab("show");
+
+  $("body").on("click", ".nav-tabs a", function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    $(this).tab("show");
+  });
+});
